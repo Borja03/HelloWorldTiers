ca9feed026e8ba2d93a1ceb6b7a1cfef2ee39846
2dam <2dam@HZ375755.TartangaLH.eus>
2024-09-16 10:23:26 +0200

    first commit

---
+++ src/dataAccess/DBUser.sql
@@ -0,0 +1,19 @@
+/**
+ * Author:  2dam
+ * Created: 16-sep-2024
+ */
+
+Create database DBUser;
+
+use DBUser;
+
+Create table user (
+    dni varchar (9) primary key,
+    username varchar (20),
+    password varchar (20),
+    fullName varchar (30)
+)
+
+insert into user (dni,username,password,fullname)
+
+
