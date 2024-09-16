91c948cc3d9daa6c1835ca5124b8090fa90dc516
Borja03 <162575763+Borja03@users.noreply.github.com>
2024-09-16 11:47:25 +0200

    Add files via upload

---
+++ build/classes/model/User.java
@@ -0,0 +1,46 @@
+package model;
+
+/**
+ *
+ * @author 2dam
+ */
+public class User {
+    private String dni;
+    private String username;
+    private String password;
+    private String fullName;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    
+   
+}
